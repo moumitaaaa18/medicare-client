7df444ca68c7585e90c94a853196af16e2a4af8d
moumitaaaa18 <dharchowdhurymoumita812@gmail.com>
2026-06-27 12:50:11 +0600

    update production

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://medicare-server-w2xv.onrender.com",
+  baseURL: "https://medicare-server-w2xv.onrender.com",
 });
