8ba1533681320f66b903446ffa220b6f6a96f23e
moumitaaaa18 <dharchowdhurymoumita812@gmail.com>
2026-06-28 20:24:38 +0600

    added sidebar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 const MainLayout = () => {
   return (
     <div className="min-h-screen bg-slate-50 text-slate-900">
+      <ScrollToTop/>
       <Navbar />
       <Outlet />
       <Footer />
